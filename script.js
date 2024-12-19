f467ebc32d52cdaf00f06b703cf86e44520d30d2
neha1562005 <76821812+neha1562005@users.noreply.github.com>
2024-12-19 12:51:28 +0600

    Add basic styling to buttons and cards and fix modal psoition

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function Book(title, author, pages, status) {
     this.author = author;
     this.pages = pages;
     this.status = status;
-    // this.info = `${title} by ${author}, ${pages} pages, ${status}`;
 };
 
 Book.prototype.toggleStatus = function () {
@@ -27,53 +26,35 @@ function displaybooks(title, author, pages, status) {
     addBookToLibrary(title, author, pages, status);
 
     const display = document.createElement('div');
-    // const buttons = document.createElement('div')
-    const removeButton = document.createElement('div');
-    // const changeStatus = document.createElement('div');
-
-    display.style.cssText = "display: flex; flex-direction: column; gap: 10px; padding: 26px; justify-content: space-between; align-items: center;";
-    removeButton.style.cssText = "padding: 10px; width: 60px; background-color: palegreen; font-size: 16px;";
-    removeButton.textContent = "Remove";
-    // changeStatus.style.cssText = "padding: 10px; width: 90px; background-color: pink; font-size: 16px;";
-    // changeStatus.textContent = "Toggle Read Status";
-    // buttons.style.cssText = "display: flex; gap: 10px; align-items: center;";
-    const lastAdded = myLibrary.at(-1);
-   
-
     const bktitle = document.createElement('div');
     const bkauthor = document.createElement('div');
     const bkpages = document.createElement('div');
     const bkstatus = document.createElement('button');
-    bkstatus.style.cssText = 'padding: 10px; width: 90px; background-color: pink; border:none; font-size: 16px;';
+    const removeButton = document.createElement('button');
+
+    display.style.cssText = "display: flex; flex-direction: column; gap: 10px; padding: 30px; justify-content: space-between; align-items: center;";
+    bkstatus.style.cssText = 'padding: 10px; width: 120px; background-color: pink; border:none; font-size: 16px; border-radius: 5px;';
+    removeButton.style.cssText = "padding: 10px; width: 120px; background-color: palegreen; font-size: 16px; border: none; border-radius: 5px;";
+
+    const lastAdded = myLibrary.at(-1);
 
     bktitle.textContent = `${lastAdded.title}`;
     bkauthor.textContent = `${lastAdded.author}`;
     bkpages.textContent = `${lastAdded.pages} pages`;
     bkstatus.textContent = `${lastAdded.status}`;
+    removeButton.textContent = "Remove";
 
+    display.setAttribute('data', myLibrary.length - 1);
+    removeButton.setAttribute('data', myLibrary.length - 1);
     bkstatus.setAttribute('data', myLibrary.length - 1);
 
     display.appendChild(bktitle);
     display.appendChild(bkauthor);
     display.appendChild(bkpages);
     display.appendChild(bkstatus);
+    display.appendChild(removeButton);
 
     displayBooks.appendChild(display);
-
-    // display.innerHTML = `<div>${lastAdded.title}</div>
-    //                     <div>${lastAdded.author}</div>
-    //                     <div>${lastAdded.pages} pages</div>
-    //                     <button style='padding: 10px; width: 90px; background-color: pink; border:none; font-size: 16px;'>${lastAdded.status}</button>
-    //                     `;
-
-
-    display.setAttribute('data', myLibrary.length - 1);
-    removeButton.setAttribute('data', myLibrary.length - 1);
-    // changeStatus.setAttribute('data', myLibrary.length - 1);
-
-    // display.appendChild(buttons);
-    display.appendChild(removeButton);
-    // displayBooks.appendChild(changeStatus);
 
     removeButton.onclick = () => {
         myLibrary.splice(removeButton.getAttribute('data'), 1);
@@ -85,15 +66,7 @@ function displaybooks(title, author, pages, status) {
         myLibrary[index].toggleStatus();
         bkstatus.textContent = `${myLibrary[index].status}`;
     };
-
-    // changeStatus.onclick = () => {
-    //     myLibrary[changeStatus.getAttribute('data')].toggleStatus();
-    //     // console.table(myLibrary);
-    // };
 };
-
-displaybooks('The Apothecary Diaries', 'Natsu Hyuga', '228', 'Read');
-displaybooks('The Hobbit', 'J.R.R. Tolkien', '295', 'Not Read');
 
 addButton.addEventListener('click', () => {
     dialog.showModal();
@@ -107,16 +80,16 @@ submitButton.addEventListener('click', (event) => {
     const pages = document.querySelector('#pages').value;
     const status = document.querySelector('input[name="bookstatus"]:checked').value;
 
-    // console.log(`${title}, ${author}, ${pages}, ${status}`);
-
     if (title && author && pages) {
         form.reset();
         dialog.close();
         displaybooks(title, author, pages, status);
     }
-
 });
 
-
+displaybooks('The Apothecary Diaries Vol 7', 'Natsu Hyuuga', '228', 'Read');
+displaybooks('Anxious People', 'Fredrik Backman', '343', 'Not Read');
+displaybooks('Before The Coffee Gets Cold', 'Toshikazu Kawaguchi', '272', 'Read');
+displaybooks('Tomorrow, and Tomorrow, and Tomorrow', 'Gabrielle Zevin', '416', 'Read');
 
 
